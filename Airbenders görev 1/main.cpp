be0d643a1bb48f5a399a94b63eacd43cdecc8a02
Kerem Giray Özcan <79506462+girayovski@users.noreply.github.com>
2022-11-07 14:57:47 +0300

    Add files via upload

---
+++ Airbenders görev 1/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+   using namespace std;
+#define G  9.80665
+
+int main() {
+    double t , x;
+        cin >> t ;
+
+    x= 0.5 * G * (t*t) ;
+     cout<< x << endl;
+     return 0;
+
+
+
+
+
+}
